a13f2ca0153bcf692cb55ac9d196a40b3c8b0810
yusheng929 <2624367622@qq.com>
2024-08-05 21:36:18 +0800

    feat: 新增

--- apps/GroupOther.js
+++ apps/GroupOther.js
@@ -3,12 +3,12 @@ import { karin, logger, segment } from 'node-karin'
 /**
  * 看头像
  */
-export const SeeImg = karin.command(/^#看头像/, async (e) => {
+export const SeeImg = karin.command(/^#看(群)?头像/, async (e) => {
 let userId = ''
  if (e.at.length) {
     userId = e.at[0]
   } else {
-    userId = e.msg.replace(/#看头像/, '').trim()
+    userId = e.msg.replace(/#看(群)?头像/, '').trim()
   }
 
   if (!userId || !(/\d{5,}/.test(userId))) {
@@ -17,4 +17,15 @@ let userId = ''
   }
   let Img = e.bot.getAvatarUrl(userId, 640);
   e.reply(segment.image(Img))
+  return true
 }, { name: "看头像", priority: "-1" })
+
+/**
+ * 看群头像
+ */
+export const SeeGroupImg = karin.command(/^#(看|取)群头像/, async (e) => {
+let groupId = e.msg.replace(/^#?(看|取)群头像/, "").trim() || e.group_id
+  let Img = e.bot.getGroupAvatar(groupId, 640);
+  e.reply(segment.image(Img))
+  return true
+}, { name: "看群头像", priority: "-1" })
