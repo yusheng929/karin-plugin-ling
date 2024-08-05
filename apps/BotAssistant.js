e931ea5f720a51c61462f2b0367e773e2cd9555b
yusheng929 <2624367622@qq.com>
2024-08-05 23:39:36 +0800

    feat: 新增改群名

--- apps/BotAssistant.js
+++ apps/BotAssistant.js
@@ -9,8 +9,9 @@ let groupId = e.msg.replace(/#?退群/g, "").trim()
     if (!groupId) return e.reply("群号不能为空")
 
     if (!/^\d+$/.test(groupId)) return e.reply("请输入正确的群号")
-    const res = await e.bot.GetGroupInfo(groupId)
-    if (!res) {
+    try {
+    await e.bot.GetGroupInfo(groupId)
+    } catch (error) {
       await e.reply('\n你好像没加入这个群', { at: true })
       return true
     }
@@ -27,3 +28,33 @@ let groupId = e.msg.replace(/#?退群/g, "").trim()
   }
   return true
 }, { name: "退群", priority: "-1" })
+
+/**
+ * 看群头像
+**/
+export const SeeImg = karin.command(/^#(看|取)头像/, async (e) => {
+let userId = ''
+ if (e.at.length) {
+    userId = e.at[0]
+  } else {
+    userId = e.msg.replace(/#(看|取)头像/, '').trim()
+  }
+
+  if (!userId || !(/\d{5,}/.test(userId))) {
+    await e.reply('\n貌似这个QQ号不对哦~', { at: true })
+    return true
+  }
+  let Img = e.bot.getAvatarUrl(userId, 640);
+  e.reply(segment.image(Img))
+  return true
+}, { name: "看头像", priority: "-1" })
+
+/**
+ * 看群头像
+ */
+export const SeeGroupImg = karin.command(/^#(看|取)群头像/, async (e) => {
+let groupId = e.msg.replace(/^#?(看|取)群头像/, "").trim() || e.group_id
+  let Img = e.bot.getGroupAvatar(groupId, 640);
+  e.reply(segment.image(Img))
+  return true
+}, { name: "看群头像", priority: "-1" })
--- apps/GroupOther.js
+++ apps/GroupOther.js
@@ -1,31 +1,27 @@
 import { karin, logger, segment } from 'node-karin'
 
 /**
- * 看头像
+ * 改群名
  */
-export const SeeImg = karin.command(/^#看(群)?头像/, async (e) => {
-let userId = ''
- if (e.at.length) {
-    userId = e.at[0]
-  } else {
-    userId = e.msg.replace(/#看(群)?头像/, '').trim()
-  }
-
-  if (!userId || !(/\d{5,}/.test(userId))) {
-    await e.reply('\n貌似这个QQ号不对哦~', { at: true })
+export const SeeImg = karin.command(/^#改群名/, async (e) => {
+  if (!(['owner', 'admin'].includes(e.sender.role) || e.isMaster)) {
+    await e.reply('暂无权限，只有管理员才能操作')
     return true
   }
-  let Img = e.bot.getAvatarUrl(userId, 640);
-  e.reply(segment.image(Img))
-  return true
-}, { name: "看头像", priority: "-1" })
+  const info = await e.bot.GetGroupMemberInfo(e.group_id, e.self_id)
+  if (!(['owner', 'admin'].includes(info.role))) {
+    await e.reply('少女做不到呜呜~(>_<)~')
+    return true
+  }
+    let Name = e.msg.replace(/^#改群名/, '').trim()
+    if (!Name) return e.reply('群名不能为空', { at: true })
 
-/**
- * 看群头像
- */
-export const SeeGroupImg = karin.command(/^#(看|取)群头像/, async (e) => {
-let groupId = e.msg.replace(/^#?(看|取)群头像/, "").trim() || e.group_id
-  let Img = e.bot.getGroupAvatar(groupId, 640);
-  e.reply(segment.image(Img))
+   try {
+    await e.bot.ModifyGroupName(e.group_id, Name)
+    e.reply(`已经将群名修改为『${Name}』`)
+  } catch (error) {
+    await e.reply('\n错误: 未知原因❌', { at: true })
+    return true
+  }
   return true
-}, { name: "看群头像", priority: "-1" })
+}, { name: "改群名", priority: "-1" })
