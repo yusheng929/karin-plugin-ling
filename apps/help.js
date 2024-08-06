8379e9ed2d76bb821e8af44664849fefcf77d026
瑜笙 <2624367622@qq.com>
2024-08-06 12:04:29 +0800

    fix: 新增#帮助

---
+++ apps/help.js
@@ -0,0 +1,6 @@
+import { karin, segment, common } from 'node-karin'
+
+export const help = karin.command(/^#?群管帮助/, async (e) => {
+ e.reply(segment.image(`file://karin-plugin-group/resources/help/help.jpg`))
+    return true
+  }
