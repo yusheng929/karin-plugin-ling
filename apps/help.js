73953876b2612dec79a1968aef1336f332f80b1e
瑜笙 <2624367622@qq.com>
2024-08-06 12:29:49 +0800

    fix: 帮助图报错

--- apps/help.js
+++ apps/help.js
@@ -3,4 +3,4 @@ import { karin, segment, common } from 'node-karin'
 export const help = karin.command(/^#?群管帮助/, async (e) => {
  e.reply(segment.image(`file://karin-plugin-group/resources/help/help.jpg`))
     return true
-  }
+  })
