d308c2d41dd66922367fa1b40c76c12b4080ebdd
yusheng929 <2624367622@qq.com>
2024-08-07 14:41:57 +0800

    fix: add .gitignore

--- apps/GroupOther.js
+++ apps/GroupOther.js
@@ -49,26 +49,3 @@ export const MuteList = karin.command(/^#?(获取|查看)?禁言列表$/, async 
   await e.bot.sendForwardMessage(e.contact, content)
   return true
 })
-
-export const ModifyMemberCard = karin.command(/^#改群名片/, async (e) => {
-  if (!(['owner', 'admin'].includes(e.sender.role) || e.isMaster)) {
-    await e.reply('暂无权限，只有管理员才能操作')
-    return true
-  }
-  const info = await e.bot.GetGroupMemberInfo(e.group_id, e.self_id)
-  if (!(['owner', 'admin'].includes(info.role))) {
-    await e.reply('少女做不到呜呜~(>_<)~')
-    return true
-  }
-  const Name = e.msg.replace(/^#改群名片/, '').trim()
-  if (!Name) return e.reply('群名不能为空', { at: true })
-
-  try {
-    await e.bot.ModifyGroupName(e.group_id, Name)
-    e.reply(`已经将群名修改为『${Name}』`)
-  } catch (error) {
-    await e.reply('\n错误: 未知原因❌', { at: true })
-    return true
-  }
-  return true
-}, { name: '改群名', priority: '-1' })
