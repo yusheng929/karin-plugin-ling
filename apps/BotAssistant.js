cd60981bdbbba5866809695031c4e1a8cdabdc69
yusheng929 <2624367622@qq.com>
2024-08-07 14:28:17 +0800

    fix: 修复#退群的权限问题

--- apps/BotAssistant.js
+++ apps/BotAssistant.js
@@ -4,30 +4,29 @@ import { karin, segment, common } from 'node-karin'
  * 退群
  */
 export const QuitGroup = karin.command(/^#?退群/, async (e) => {
-  const groupId = e.msg.replace(/#?退群/g, '').trim()
+  let group_id = e.msg.replace(/#?退群/g, '').trim()
+  if (!group_id) group_id = e.group_id
 
-  if (!groupId) return e.reply('群号不能为空')
-
-  if (!/^\d+$/.test(groupId)) return e.reply('请输入正确的群号')
+  if (!/^\d+$/.test(group_id)) return e.reply('请输入正确的群号')
   try {
-    await e.bot.GetGroupInfo(groupId)
+    await e.bot.GetGroupInfo(group_id)
   } catch (error) {
     await e.reply('\n你好像没加入这个群', { at: true })
     return true
   }
   try {
-    if (groupId === e.group_id) {
+    if (group_id === e.group_id) {
       await e.reply('3秒后退出本群聊')
       await common.sleep(3000)
     }
-    await e.bot.LeaveGroup(groupId)
-    e.reply(`已退出群聊『${groupId}』`)
+    await e.bot.LeaveGroup(group_id)
+    e.reply(`已退出群聊『${group_id}』`)
   } catch (error) {
     await e.reply('\n错误: 未知原因❌', { at: true })
     return true
   }
   return true
-}, { name: '退群', priority: '-1' })
+}, { name: "退群", priority: "-1", permission: "master" })
 
 /**
  * 看群头像
@@ -53,8 +52,8 @@ export const SeeImg = karin.command(/^#(看|取)头像/, async (e) => {
  * 看群头像
  */
 export const SeeGroupImg = karin.command(/^#(看|取)群头像/, async (e) => {
-  const groupId = e.msg.replace(/^#?(看|取)群头像/, '').trim() || e.group_id
-  const Img = e.bot.getGroupAvatar(groupId, 640)
+  const group_id = e.msg.replace(/^#?(看|取)群头像/, '').trim() || e.group_id
+  const Img = e.bot.getGroupAvatar(group_id, 640)
   e.reply(segment.image(Img))
   return true
 }, { name: '看群头像', priority: '-1' })
--- apps/进退群通知.js
+++ apps/进退群通知.js
@@ -1,6 +1,6 @@
 import { karin, redis } from 'node-karin'
-import { Config } from '../lib/index.js'
-
+import { Config, YamlReader } from '../lib/index.js'
+let cfgPath = 'plugins/karin-plugin-group/config/config/other.yaml'
 /**
  * 进群通知
  */
@@ -12,7 +12,7 @@ export const accept = karin.accept('notice.group_member_increase', async (e) => 
     await redis.set(key, "1", { EX: Config.Other.accept.cd });
   await e.reply('\n欢迎加入本群୯(⁠*⁠´⁠ω⁠｀⁠*⁠)୬', { at: true })
   return true
-})
+}, { name: "进群通知", priority: "-1" })
 
 /**
  * 退群通知
@@ -21,4 +21,46 @@ export const unaccept = karin.accept('notice.group_member_decrease', async (e) =
    if (Config.Other.accept.BlackGroup.some(i => i === e.group_id)) return false
   await e.reply(`用户『${e.user_id}』丢下我们一个人走了(╥_╥)`)
   return true
-})
+}, { name: "退群通知", priority: "-1"})
+
+export const CloseNotification = karin.command(/^#关闭进群通知/, async (e) => {
+  let group_id = e.msg.replace(/#关闭进群通知/g, '').trim()
+  if (!group_id) group_id = e.group_id
+  if (!/^\d+$/.test(group_id)) return e.reply('请输入正确的群号')
+   try {
+      const yaml = new YamlReader(cfgPath)
+      const data = yaml.get('accept.BlackGroup')
+      if (!data?.includes?.(group_id)) {
+        if (!data || !Array.isArray(data)) {
+          yaml.set('accept.BlackGroup', [ group_id ])
+        } else {
+          yaml.addIn('accept.BlackGroup', group_id)
+        }
+        await e.reply(`已经关闭群『${group_id}』的进群通知`)
+      } else {
+        await e.reply(`群『${group_id}』的进群通知已经处于关闭状态啦！`)
+      }
+    } catch (error) {
+      await e.reply(`失败: 未知错误❌`)
+      logger.error(error)
+    }
+}, { name: "关闭进群通知", priority: "-1", permission: "master" })
+
+export const ActivateNotification = karin.command(/^#开启进群通知/, async (e) => {
+  let group_id = e.msg.replace(/#开启进群通知/g, '').trim()
+  if (!group_id) group_id = e.group_id
+  if (!/^\d+$/.test(group_id)) return e.reply('请输入正确的群号')
+   try {
+      const yaml = new YamlReader(cfgPath)
+      const data = yaml.get('accept.BlackGroup')
+      if (Array.isArray(data) && data.includes(group_id)) {
+        Config.modify('other', 'accept.BlackGroup', group_id)
+        await e.reply(`已经开启群『${group_id}』的进群通知`)
+      } else {
+        await e.reply(`群${group_id}的进群通知已经处于开启状态啦！`)
+      }
+    } catch (error) {
+      await e.reply(`错误:未知原因❌`)
+      logger.error(error)
+    }
+}, { name: "关闭进群通知", priority: "-1", permission: "master" })
