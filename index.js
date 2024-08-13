869ea4b6232e6b72c89002a06149759f5489a695
yusheng929 <2624367622@qq.com>
2024-08-13 15:58:59 +0800

    fix: 优化插件加载

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-import path from 'path'
-import { logger, common } from 'node-karin'
+import { Version } from './lib/index.js'
+import { logger } from 'node-karin'
 
-/** 当前文件的绝对路径 */
-const filePath = common.absPath(import.meta.url.replace(/^file:(\/\/\/|\/\/)/, ''))
-/** 插件包的目录路径 */
-const dirname = path.dirname(filePath)
-/** 插件包的名称 */
-const basename = path.basename(dirname)
-/** 插件包相对路径 */
-const dirPath = './plugins/' + basename
+logger.info('-----------------')
+logger.info(`${Version.pluginName}${Version.version}初始化~`)
+logger.info('-------^_^-------')
 
-export { dirPath }
-
-logger.info(basename + ' 插件 0.0.1初始化~')
+export * from './apps/index.js'
