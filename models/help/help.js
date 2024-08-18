49787761011b983f18e5a1827ec14b0aa221b064
yusheng929 <2624367622@qq.com>
2024-08-18 13:14:33 +0800

    fix: 新增帮助图

--- models/help/help.js
+++ models/help/help.js
@@ -23,9 +23,39 @@ export const helpList = [
       },
       {
         icon: 3,
-        title: '#踢@114514',
+        title: '#解禁<@QQ>',
         desc: '字面意思'
       },
+      {
+        icon: 4,
+        title: '#踢<@QQ>',
+        desc: '字面意思'
+      },
+      {
+        icon: 8,
+        title: '#获取禁言列表',
+        desc: '获取本群被禁言的人'
+      },
+    ]
+  },
+  {
+    group: 'Bot为群主可用',
+    list: [
+      {
+        icon: 5,
+        title: '#申请头衔<头衔>',
+        desc: '字面意思'
+      },
+      {
+        icon: 6,
+        title: '#设置管理<@QQ>',
+        desc: '字面意思'
+      },
+      {
+        icon: 7,
+        title: '#取消管理<@QQ>',
+        desc: '字面意思'
+      }
     ]
   }
 ]
