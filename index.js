5b4b822a3f3180929316e642a82f9bc376988adc
yusheng929 <2624367622@qq.com>
2024-08-19 15:40:29 +0800

    fix: 修改package.json

--- index.js
+++ index.js
@@ -4,5 +4,3 @@ import { logger } from 'node-karin'
 logger.info('-----------------')
 logger.info(`${Version.pluginName}${Config.package.version}初始化~`)
 logger.info('-------^_^-------')
-
-export * from './apps/index.js'
