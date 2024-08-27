8603adbc9f93ce0d07ca6ecab8db15e9f6381a0c
岩浆 <linboyan5438081@163.com>
2024-08-27 08:57:44 +0800

    feat: automatically agree group request (#14)

--- apps/进退群通知.js
+++ apps/进退群通知.js
@@ -1,4 +1,4 @@
-import { karin, YamlEditor } from 'node-karin'
+import { karin, YamlEditor, Cfg, segment } from 'node-karin'
 import { Config } from '../components/index.js'
 
 const groupState = {}
@@ -96,10 +96,88 @@ const updateNotificationStatus = async (e, action, successMessage, alreadyMessag
   }
 }
 
+export const deal_invited_group = karin.accept('request.invited_group',
+  async (e) => {
+    const opts = Config.Other.DealRequest.InvitedJoinGroup
+    if (opts.accept) {
+      await e.bot.SetInvitedJoinGroupResult(e.raw_event.flag, true)
+    }
+    if (!opts.notify) return
+    const AvatarUrl = await e.bot.getGroupAvatarUrl(e.group_id)
+    Cfg.master.concat(Cfg.admin).forEach(master => {
+      try {
+        e.bot.SendMessage({ scene: 'friend', peer: master }, [
+          segment.image(AvatarUrl),
+          segment.text(`接到群『${e.group_id}』的拉群申请， ${opts.accept ? '已处理，' : ''}识别号: ${e.raw_event.flag}`)
+        ])
+      } catch (error) { }
+    })
+  },
+  {
+    name: '处理拉群申请',
+    priority: 100,
+    log: true
+  }
+)
+
+export const deal_private_apply = karin.accept('request.private_apply',
+  async (e) => {
+    const opts = Config.Other.DealRequest.Friend
+    if (opts.accept) {
+      await e.bot.SetFriendApplyResult(e.raw_event.flag, true)
+    }
+    if (!opts.notify) return
+    const AvatarUrl = await e.bot.getAvatarUrl(e.user_id)
+    Cfg.master.concat(Cfg.admin).forEach(master => {
+      try {
+        e.bot.SendMessage({ scene: 'friend', peer: master }, [
+          segment.image(AvatarUrl),
+          segment.text(`接到${e.sender.nick || ''}『${e.user_id}』的好友申请， ${opts.accept ? '已处理，' : ''}识别号: ${e.raw_event.flag}${e.content.message ? '\n申请理由: ' + e.content.message : ''}`)
+        ])
+      } catch (error) { }
+    })
+  },
+  {
+    name: '处理加好友申请',
+    priority: 100,
+    log: true
+  }
+)
+
+export const deal_group_apply = karin.accept('request.group_apply',
+  async (e) => {
+    const opts = Array.isArray(Config.Other.DealRequest.Group) ? Config.Other.DealRequest.Group.find(group => group[e.group_id])[e.group_id] : Config.Other.DealRequest.Group
+    if (!opts) return
+    if (opts.accept) {
+      await e.bot.SetGroupApplyResult(e.raw_event.flag, true)
+    }
+    if (!opts.notify) return
+    const AvatarUrl = await e.bot.getAvatarUrl(e.user_id)
+    const GroupAvatarUrl = await e.bot.getGroupAvatarUrl(e.group_id)
+    Cfg.master.concat(Cfg.admin).forEach(master => {
+      try {
+        e.bot.SendMessage({ scene: 'friend', peer: master }, [
+          segment.image(GroupAvatarUrl),
+          segment.text(`接到群『${e.group_id}』的加群申请，申请人${e.sender.nick || ''}『${e.user_id}』， ${opts.accept ? '已处理，' : ''}识别号: ${e.raw_event.flag}${e.content.message ? '\n申请理由: ' + e.content.message : ''}`)
+        ])
+      } catch (error) { }
+    })
+    e.bot.SendMessage({ scene: 'group', peer: e.group_id }, [
+      segment.image(AvatarUrl),
+      segment.text(`接到${e.sender.nick || ''}『${e.user_id}』的加群申请。 ${opts.accept ? '已处理。' : ''}${e.content.message ? '\n申请理由: ' + e.content.message : ''}`)
+    ])
+  },
+  {
+    name: '处理加群申请',
+    priority: 100,
+    log: true
+  }
+)
+
 export const CloseNotification = karin.command(/^#关闭进群通知/, async (e) => {
   return await updateNotificationStatus(e, 'add', '已经关闭群『{group_id}』的进群通知', '群『{group_id}』的进群通知目前已经处于关闭状态啦，无需重复关闭！')
-}, { name: '关闭进群通知', priority: '-1', permission: 'master' })
+}, { name: '关闭进群通知', priority: '-1', permission: 'group.admin' })
 
 export const ActivateNotification = karin.command(/^#开启进群通知/, async (e) => {
   return await updateNotificationStatus(e, 'remove', '已经开启群『{group_id}』的进群通知', '群『{group_id}』的进群通知目前已经处于开启状态啦，无需重复开启！')
-}, { name: '开启进群通知', priority: '-1', permission: 'master' })
+}, { name: '开启进群通知', priority: '-1', permission: 'group.admin' })
