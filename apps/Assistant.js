76beebf9b391db6b8500551888da9dbcf6d7a5b8
yusheng929 <2624367622@qq.com>
2024-08-27 14:08:56 +0800

    fix: 新增黑白名单开关

--- apps/Assistant.js
+++ apps/Assistant.js
@@ -1,67 +1,8 @@
 import { karin, YamlEditor, config } from 'node-karin'
+import { 编辑文件 } from '#lib'
 
-/**
- * 更新群组通知状态
- * @param e 事件对象
- * @param type 类型 ('White' 或 'Black')
- * @param isRemoval 是否是移除操作
- * @param targetType 目标类型 ('Group' 或 'User')
- * @param id 目标 ID
- * @returns 是否继续处理
- */
-const Edit = async (e, type, isRemoval, targetType, id) => {
-  try {
-    const yaml = new YamlEditor('config/config/config.yaml')
-    let data
-
-    if (type === 'Black') {
-      data = targetType === 'Group' ? yaml.get('BlackList.groups') : yaml.get('BlackList.users')
-    } else if (type === 'White') {
-      data = targetType === 'Group' ? yaml.get('WhiteList.groups') : yaml.get('WhiteList.users')
-    }
-
-    if (!Array.isArray(data)) {
-      await e.reply('\n配置文件格式错误❌', { at: true })
-      return true
-    }
-
-    const isIdInList = data.includes(id)
-
-    if ((!isRemoval && isIdInList) || (isRemoval && !isIdInList)) {
-      await e.reply(`${targetType === 'Group' ? "群" : "用户"}『${id}』已经在${type === 'White' ? "白名单" : "黑名单"}里面了`)
-      return true
-    }
-
-    if (!isRemoval) {
-      if (targetType === 'Group') {
-        yaml.append(`${type === 'White' ? 'WhiteList.groups' : 'BlackList.groups'}`, String(id))
-        if ((type === 'White' && !config.App.WhiteList.groups) || type === 'Black' && !config.App.WhiteList.groups) e.reply(`当前${type === 'White' ? "白" : "黑"}名单中的群配置未开启，黑白名单将无效，可前往config/config/App.yaml中启用`)
-      } else {
-        yaml.append(`${type === 'White' ? 'WhiteList.users' : 'BlackList.users'}`, String(id))
-        if ((type === 'White' && !config.App.WhiteList.users) || type === 'Black' && !config.App.WhiteList.users) e.reply(`当前${type === 'White' ? "白" : "黑"}名单中的用户配置未开启，黑白名单将无效，可前往config/config/App.yaml中启用`)
-      }
-      await e.reply(`已经将${targetType === 'Group' ? "群" : "用户"}『${id}』${type === 'White' ? "拉白" : "拉黑"}掉了`)
-    } else {
-      if (targetType === 'Group') {
-        yaml.remove(`${type === 'White' ? 'WhiteList.groups' : 'BlackList.groups'}`, String(id))
-      } else {
-        yaml.remove(`${type === 'White' ? 'WhiteList.users' : 'BlackList.users'}`, String(id))
-      }
-      await e.reply(`已经将${targetType === 'Group' ? "群" : "用户"}『${id}』取消${type === 'White' ? "拉白" : "拉黑"}了`)
-    }
-
-    yaml.save()
-    return true
-  } catch (error) {
-    await e.reply('失败: 未知错误❌')
-    console.error(error)
-    return true
-  }
-}
-
-export const BanorWh = karin.command(/^#(取消)?(拉黑|拉白)(群)?/, async (e) => {
+export const 黑白名单 = karin.command(/^#(取消)?(拉黑|拉白)(群)?/, async (e) => {
   let id
-
   if (!e.msg.includes('群')) {
     id = e.at.length ? e.at[0] : e.msg.replace(/#(取消)?(拉黑|拉白)/, '').trim()
   } else {
@@ -74,5 +15,29 @@ export const BanorWh = karin.command(/^#(取消)?(拉黑|拉白)(群)?/, async (
   const isRemoval = e.msg.includes('取消')
   const targetType = e.msg.includes('群') ? 'Group' : 'User'
 
-  return await Edit(e, type, isRemoval, targetType, id)
+  return await 编辑文件.编辑黑白名单(e, type, isRemoval, targetType, id)
 }, { name: '取消拉黑拉白群', priority: '-1', permission: 'master' })
+
+export const 修改日志等级 = karin.command(/^#(修改|设置)日志等级/, async (e) => {
+  const 日志等级列表 = [ "trace", "debug", "info", "warn", "fatal", "mark", "error", "off" ]
+  const 日志等级 = e.msg.replace(/^#?(设置|修改)日志等级/, "").trim()
+  if (!日志等级列表.includes(日志等级)) return e.reply("❎ 请输入正确的参数，可选：\ntrace,debug,info,warn,fatal,mark,error,off")
+  return await 编辑文件.编辑文件(e, 'config.yaml', 日志等级, 'log4jsCfg.level', '日志等级')
+}, { name: '修改日志等级', priority: '-1', permission: 'master' })
+
+export const 修改黑白名单 = karin.command(/^#(开启|关闭)(黑|白)名单(群)?$/, async (e) => {
+  let 修改值 = e.msg.includes('开启') ? true : false
+  
+  let 配置项
+  
+  if (e.msg.includes('群')){
+  配置项 = e.msg.includes('黑') ? 'BlackList.groups' : 'WhiteList.groups'
+  } else {
+  配置项 = e.msg.includes('黑') ? 'BlackList.users' : 'WhiteList.users'
+  }
+  
+  let 修改内容 = e.msg.includes('黑') ? '黑名单' : '白名单'
+  
+  return await 编辑文件.编辑文件(e, 'App.yaml', 修改值, 配置项, 修改内容)
+  
+}, { name: '修改黑白名单配置', priority: '-1', permission: 'master' })
