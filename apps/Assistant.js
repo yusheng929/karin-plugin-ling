82cacb5bf0548eea49927986542eeacc8989bb1f
yusheng929 <2624367622@qq.com>
2024-09-01 08:36:41 +0800

    fix: 新增清屏撤回

--- apps/Assistant.js
+++ apps/Assistant.js
@@ -17,7 +17,7 @@ export const 黑白名单 = karin.command(/^#(取消)?(拉黑|拉白)(群)?/, as
 
   return await 编辑文件.编辑黑白名单(e, type, isRemoval, targetType, id)
 }, { name: '取消拉黑拉白群', priority: '-1', permission: 'master' })
-export const 撤回 = karin.command(/^#撤回/, async (e) => {
+export const 撤回 = karin.command(/^#?撤回$/, async (e) => {
  if (!(['owner', 'admin'].includes(e.sender.role) || e.isMaster)) {
     await e.reply('暂无权限，只有管理员才能操作')
     return true
@@ -25,3 +25,17 @@ export const 撤回 = karin.command(/^#撤回/, async (e) => {
   e.bot.RecallMessage(e.contact, e.reply_id)
   e.bot.RecallMessage(e.contact, e.message_id)
 }, { name: '撤回', priority: '-1' })
+export const 清屏撤回 = karin.command(/^#清屏(\d+)?/, async (e) => {
+  if (!(['owner', 'admin'].includes(e.sender.role) || e.isMaster)) {
+    await e.reply('暂无权限，只有管理员才能操作')
+    return true
+  }
+
+  let match = e.msg.replace(/#清屏/, '').trim() || 50
+  let msg_ids = await e.bot.GetHistoryMessage(e.contact, e.message_id, match)
+  let msg_id_list = msg_ids.map(item => item.message_id)
+ await e.reply('开始执行清屏操作，请确保我有管理员')
+  for (const msg_id of msg_id_list) {
+    await e.bot.RecallMessage(e.contact, msg_id)
+  }
+}, { name: '清屏', priority: '-1' })
