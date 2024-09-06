b430b7bfb1d8060d08c22085b5849e3ab3d4c5f8
yusheng929 <2624367622@qq.com>
2024-09-07 06:46:35 +0800

    fix: 修复进群验证无法使用

--- apps/测试(仅测试，无功能).js
+++ apps/测试(仅测试，无功能).js
@@ -1,19 +1,25 @@
 import { karin } from 'node-karin'
 // 仅用于开发者测试部分功能实现，无其他作用
 export const test = karin.command(/^测试/, async (e) => {
+  let num = 2
+  let user_id = e.user_id
+  await e.reply(`\n为确保你不是机器人\n请在3分钟内输入下方验证码\n『${num}』`, { at: true })
   try {
-    e.reply('测试')
-    
-    // 使用 try...catch 来捕获 ctx 方法可能抛出的错误
-    const event = await karin.ctx(e, {
-      time: 10,         // 设置超时时间为10秒（为了演示，可以调节时间）
-      reply: true,
-      replyMsg: '已超时'
-    })
-    
-    // 处理正常情况下的 event
-    e.reply('成功接收到事件')
-  } catch (error) {
-    e.reply(`出现错误: ${error.message}`)
+  for (let i = 3; i >= 0; i--) {
+   const event = await karin.ctx(e, { time: 10, reply: false})
+   if (!(event.msg == num) && (i == 1) {
+   await e.reply('验证失败，你将会被踢出群聊', { at: true })
+   return true
+   }
+  if (event.msg == num) {
+  await e.reply('\n验证通过，欢迎加入群聊', { at: true })
+  return true
+  } else {
+   await e.reply(`验证码错误，请重新输入\n你还有${i - 1}次机会`)
+  }
+}
+} catch (error) {
+  await e.reply('输入超时，你将会被踢出群聊', { at: true })
+  return true
   }
 }, { name: '测试', priority: '-1' })
--- apps/进退群功能.js
+++ apps/进退群功能.js
@@ -15,12 +15,12 @@ let data1 = Config.Other.Test
   await e.reply(`\n为确保你不是机器人\n请在3分钟内输入下方验证码\n『${num}』`, { at: true })
   try {
   for (let i = 3; i >= 0; i--) {
-   if (i == 0) {
+  const event = await karin.ctx(e, { time: 120, reply: false})
+   if (i == 1 && !(event.msg == num)) {
    await e.reply('验证失败，你将会被踢出群聊', { at: true })
    await e.bot.KickMember(e.group_id, user_id)
    return true
    }
-   const event = await karin.ctx(e, { time: 120, reply: false})
   if (event.msg == num) {
   await e.reply('\n验证通过，欢迎加入群聊', { at: true })
   return true
