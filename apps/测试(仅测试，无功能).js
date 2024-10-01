b25b60ac38edc1086e524b83309143168e4dce1a
yusheng929 <you@example.com>
2024-10-01 09:58:32 +0800

    fix: 少女祈祷中

--- apps/测试(仅测试，无功能).js
+++ apps/测试(仅测试，无功能).js
@@ -1,12 +1,28 @@
 import { karin, segment, common, Cfg, YamlEditor } from 'node-karin'
 import { Config } from '#components'
 export const test = karin.command(/^测试/, async (e) => {
-let Path = e.msg.replace(/测试/, '').trim()
-e.reply('请发送文件', {at: true})
-console.log(`${process.cwd()}`)
-const event = await karin.ctx(e)
-let file = JSON.parse(event.msg)
-console.log(file)
-console.log(file.type)
-if (!(file.type === 'file')) return e.reply('未识别到文件，取消操作', {at: true})
+logger.info(`\x1b[34m                 .::::..
+      ::::rrr7QQJi::i:iirijQBBBQB.
+      BBQBBBQBP. ......:::..1BBBB
+      .BuPBBBX  .........r.  vBQL  :Y.
+       rd:iQQ  ..........7L   MB    rr
+        7biLX .::.:....:.:q.  ri    .
+         JX1: .r:.r....i.r::...:.  gi5
+         ..vr .7: 7:. :ii:  v.:iv :BQg
+         : r:  7r:i7i::ri:DBr..2S
+      i.:r:. .i:XBBK...  :BP ::jr   .7.
+      r  i....ir r7.         r.J:   u.
+     :..X: .. .v:           .:.Ji
+    i. ..i .. .u:.     .   77: si   1Q
+   ::.. .r .. :P7.r7r..:iLQQJ: rv   ..
+  7  iK::r  . ii7r LJLrL1r7DPi iJ     r
+    .  ::.:   .  ri 5DZDBg7JR7.:r:   i.
+   .Pi r..r7:     i.:XBRJBY:uU.ii:.  .
+   QB rJ.:rvDE: .. ri uv . iir.7j r7.
+  iBg ::.7251QZ. . :.      irr:Iu: r.
+   QB  .:5.71Si..........  .sr7ivi:U
+   7BJ .7: i2. ........:..  sJ7Lvr7s
+    jBBdD. :. ........:r... YB  Bi
+       :7j1.                 :  :\x1b[0m`);
+
 }, { name: '测试', priority: '-1' })
