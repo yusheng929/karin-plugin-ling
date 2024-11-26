c2b3529745d6d496cd2990711c47575d98a53986
yusheng929 <2624367622@qq.com>
2024-11-26 10:32:12 +0800

    chore: 新增编码解码

---
+++ lib/apps/转码编码.d.ts
@@ -0,0 +1,4 @@
+export declare const url: import("node-karin").CommandInfo;
+export declare const domain: import("node-karin").CommandInfo;
+export declare const base64: import("node-karin").CommandInfo;
+export declare const md5: import("node-karin").CommandInfo;
