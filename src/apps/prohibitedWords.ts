5f62d985df4f4970ec9a4f3a8927137643e30248
时瑾 <74231782+sj817@users.noreply.github.com>
2024-12-24 09:07:04 +0800

    feat: 重构违禁词查看功能，优化消息格式并增强权限检查

--- src/apps/prohibitedWords.ts
+++ src/apps/prohibitedWords.ts
@@ -1,57 +1,43 @@
-import { karin, segment, common, KarinElement } from 'node-karin'
-import { Config } from '@/components'
+import { group } from '@/utils/config'
+import { karin, segment, common } from 'node-karin'
 
 export const ProhibitedWords = karin.command(/^#查看(所有)?违禁词/, async (e) => {
-  const data = Config.GroupYaml
+  const data = group()
   if (e.msg.includes('所有')) {
-    if (!e.isMaster) {
-      e.reply('暂无权限，只有主人才能操作')
-      return false
-    }
-    const msgs = []
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        const type = data[key]['enable']
-        const time = data[key]['time']
-        const bantype = data[key]['ban']
-        const ruleid = data[key]['rule']
-        const rule = ruleid == 0 ? '模糊拦截' : '精准拦截'
-        const words = (data[key]['words']).join('\n')
-        msgs.push([
-          segment.text(`id: ${key}`),
-          segment.text(`\n是否启用: ${type}`),
-          segment.text(`\n拦截规则: ${rule}`),
-          segment.text(`\n是否禁言: ${bantype}`),
-          segment.text(`\n禁言时长: ${time}`),
-          segment.text(`\n违禁词列表: \n${words}`),
-        ])
-      }
-    }
-    const msg = common.makeForward(msgs as unknown as KarinElement[], e.selfId, e.bot.account.name)
-    await e.bot.sendForwardMessage(e.contact, msg)
+    const list: string[] = []
+    Object.keys(data).forEach((id) => {
+      list.push([
+        `群号: ${id}`,
+        `是否启用: ${data[id]['enable']}`,
+        `拦截规则: ${Number(data[id]['rule']) === 0 ? '模糊拦截' : '精准拦截'}`,
+        `是否禁言: ${data[id]['ban']}`,
+        `禁言时长: ${data[id]['time']}`,
+        `违禁词列表: ${data[id]['words'].join('\n')}`,
+      ].join('\n'))
+    })
+
+    const message = list.map((v) => segment.text(v))
+    const msg = common.makeForward(message, e.selfId, e.bot.account.name)
+    await e.bot.sendForwardMsg(e.contact, msg)
     return true
   }
-  let datas = e.groupId
-  datas = data[`${datas}`] ? e.groupId : 'default'
-  const rules = (data[`${datas}`] && data[`${datas}`]['words']) || ''
-  if (!rules) {
-    e.reply('暂无违禁词')
+
+  if (!e.isGroup) {
+    await e.reply('请在群聊中使用', { at: true })
     return false
   }
-  const rule = rules.join('\n')
-  const msgs = []
-  const type = data[`${datas}`]['enable']
-  const bantype = data[`${datas}`]['ban']
-  const time = data[`${datas}`]['time']
-  const types = data[`${datas}`]['rule']
-  const typess = types == 0 ? '模糊拦截' : '精准拦截'
-  msgs.push([segment.text(`是否启用: ${type}`)])
-  msgs.push([segment.text(`拦截规则: ${typess}`)])
-  msgs.push([segment.text(`是否禁言: ${bantype}`)])
-  msgs.push([segment.text(`禁言时长: ${time}`)])
-  msgs.push([segment.text(`违禁词: \n${rule}`)])
-  if (datas == 'default') msgs.unshift(segment.text('当前群为配置违禁词，将使用默认违禁词'))
-  const msg = common.makeForward(msgs as unknown as KarinElement[], e.selfId, e.bot.account.name)
-  await e.bot.sendForwardMessage(e.contact, msg)
+
+  const key = data[e.groupId] ? e.groupId : 'default'
+  const { enable, rule, ban, time, words } = data[key]
+  const message = [
+    '\n' + key === e.groupId ? `群号: ${e.groupId}` : '当前群未配置违禁词，将使用默认违禁词',
+    `是否启用: ${enable}`,
+    `拦截规则: ${Number(rule) === 0 ? '模糊拦截' : '精准拦截'}`,
+    `是否禁言: ${ban}`,
+    `禁言时长: ${time}`,
+    `违禁词列表: ${words.join('\n')}`,
+  ].join('\n')
+
+  await e.reply(message, { at: true })
   return true
-}, { name: '查看违禁词', priority: -1 })
+}, { name: '查看违禁词', priority: -1, perm: 'master' })
