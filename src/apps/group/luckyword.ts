accdf64d56a68f049ba868a2512a7589ad1dbcdc
瑜笙 <2624367622@qq.com>
2025-02-12 10:42:28 +0800

    fix: 优化幸运字符列表的消息格式，改善可读性

--- src/apps/group/luckyword.ts
+++ src/apps/group/luckyword.ts
@@ -8,13 +8,13 @@ export const luckylist = karin.command(/^#(查)?(幸运)?字符(列表)?$/, asyn
   const msg = []
   const items = data.data.word_list
   for (const item of items) {
-    msg.push([
+    msg.push(...[
       segment.image(`https://tianquan.gtimg.cn/groupluckyword/item/${item.word_info.word_id}/tinypic-0.png?m=${item.word_info.mtime}`),
-      segment.text(`id: ${item.word_info.word_id}`),
-      segment.text(`名称: ${item.word_info.wording}`),
-      segment.text(`寓意: ${item.word_info.word_desc}`),
-      segment.text(`点亮状态: ${item.light_up_info.char_count === item.light_up_info.light_up_char_count ? '已点亮' : item.light_up_info.light_up_char_count + '/' + item.light_up_info.char_count}`)
-    ].join('\n'))
+      segment.text(`\nid: ${item.word_info.word_id}`),
+      segment.text(`\n名称: ${item.word_info.wording}`),
+      segment.text(`\n寓意: ${item.word_info.word_desc}`),
+      segment.text(`\n点亮状态: ${item.light_up_info.char_count === item.light_up_info.light_up_char_count ? '已点亮' : item.light_up_info.light_up_char_count + '/' + item.light_up_info.char_count}`)
+    ])
   }
   msg.unshift(segment.text(`当前正在使用的幸运字符是\nID: ${data.data.equip_info.word_info.word_id}`))
   const content = common.makeForward(msg, e.selfId, e.bot.account.name)
