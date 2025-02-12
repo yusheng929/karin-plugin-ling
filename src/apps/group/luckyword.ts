8b8827729171f5d73a22e0ba482ed41b087c8161
瑜笙 <2624367622@qq.com>
2025-02-12 14:05:30 +0800

    fix: 优化幸运字符列表的消息构建逻辑，提升可读性

--- src/apps/group/luckyword.ts
+++ src/apps/group/luckyword.ts
@@ -5,18 +5,19 @@ export const luckylist = karin.command(/^#(查)?(幸运)?字符(列表)?$/, asyn
   const data = await new QQApi(e).luckylist(e.groupId)
   if (!data) return e.reply('❌请稍后再试')
   if (data.retcode !== 0) return e.reply('❌获取数据错误')
-  const msg = []
+  const msgs = []
   const items = data.data.word_list
   for (const item of items) {
-    msg.push(...[
-      segment.image(`https://tianquan.gtimg.cn/groupluckyword/item/${item.word_info.word_id}/tinypic-0.png?m=${item.word_info.mtime}`),
-      segment.text(`\nid: ${item.word_info.word_id}`),
-      segment.text(`\n名称: ${item.word_info.wording}`),
-      segment.text(`\n寓意: ${item.word_info.word_desc}`),
-      segment.text(`\n点亮状态: ${item.light_up_info.char_count === item.light_up_info.light_up_char_count ? '已点亮' : item.light_up_info.light_up_char_count + '/' + item.light_up_info.char_count}`)
-    ])
+    msgs.push([
+      `https://tianquan.gtimg.cn/groupluckyword/item/${item.word_info.word_id}/tinypic-0.png?m=${item.word_info.mtime}`,
+      `id: ${item.word_info.word_id}`,
+      `名称: ${item.word_info.wording}`,
+      `寓意: ${item.word_info.word_desc}`,
+      `点亮状态: ${item.word_info.char_count === item.light_up_info.light_up_char_count ? '已点亮' : item.light_up_info.light_up_char_count + '/' + item.word_info.char_count}`
+    ].join('\n'))
   }
-  msg.unshift(segment.text(`当前正在使用的幸运字符是\nID: ${data.data.equip_info.word_info.word_id}`))
+  msgs.unshift(`当前正在使用的幸运字符是\nID: ${data.data.equip_info.word_info.word_id}`)
+  const msg = msgs.map((m) => segment.text(m))
   const content = common.makeForward(msg, e.selfId, e.bot.account.name)
   await e.bot.sendForwardMsg(e.contact, content)
   return true
