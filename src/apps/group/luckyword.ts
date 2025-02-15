a5d8034195fb8326e233af7413f0406cfdfc3c08
瑜笙 <2624367622@qq.com>
2025-02-15 16:11:03 +0800

    feat: 优化幸运字符列表功能，增加图片展示和文本格式化

--- src/apps/group/luckyword.ts
+++ src/apps/group/luckyword.ts
@@ -2,7 +2,7 @@ import { render } from '@/lib/puppeteer'
 import QQApi from '@/models/api/QQApi'
 import { other } from '@/utils/config'
 import { dirPath } from '@/utils/dir'
-import karin, { common, segment } from 'node-karin'
+import karin, { common, logger, segment } from 'node-karin'
 
 export const luckylist = karin.command(/^#(查)?(幸运)?字符(列表)?$/, async (e) => {
   const data = await new QQApi(e).luckylist(e.groupId)
@@ -12,15 +12,15 @@ export const luckylist = karin.command(/^#(查)?(幸运)?字符(列表)?$/, asyn
   const items = data.data.word_list
   for (const item of items) {
     msgs.push([
-      `id: ${item.word_info.word_id}`,
-      `名称: ${item.word_info.wording}`,
-      `寓意: ${item.word_info.word_desc}`,
-      `点亮状态: ${item.word_info.char_count === item.light_up_info.light_up_char_count ? '已点亮' : item.light_up_info.light_up_char_count + '/' + item.word_info.char_count}`
-    ].join('\n'))
+      segment.image(`https://tianquan.gtimg.cn/groupluckyword/item/${item.word_info.word_id}/pic-${item.light_up_info.light_up_char_count}.png?m=${item.word_info.mtime}`),
+      segment.text(`id: ${item.word_info.word_id}`),
+      segment.text(`\n名称: ${item.word_info.wording}`),
+      segment.text(`\n寓意: ${item.word_info.word_desc}`),
+      segment.text(`\n点亮状态: ${item.word_info.char_count === item.light_up_info.light_up_char_count ? '已点亮' : item.light_up_info.light_up_char_count + '/' + item.word_info.char_count}`)
+    ])
   }
-  msgs.unshift(`当前正在使用的幸运字符是\nID: ${data.data.equip_info.word_info.word_id}`)
-  const msg = msgs.map((m) => segment.text(m))
-  const content = common.makeForward(msg, e.selfId, e.bot.account.name)
+  msgs.unshift([segment.text(`当前正在使用的幸运字符是\nID: ${data.data.equip_info.word_info.word_id}`)])
+  const content = common.makeForward(msgs, e.selfId, e.bot.account.name)
   await e.bot.sendForwardMsg(e.contact, content)
   return true
 }, { name: '幸运字符列表', priority: -1, event: 'message.group' })
