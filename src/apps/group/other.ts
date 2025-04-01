2473eb888643c41fdecec010646e82b18454a8dd
瑜笙 <2624367622@qq.com>
2025-04-01 11:55:35 +0800

    feat: 添加修改群头像功能，支持icqq适配器跟onebotv11协议

--- src/apps/group/other.ts
+++ src/apps/group/other.ts
@@ -1,6 +1,7 @@
 import { isAdmin } from '@/utils/common'
 import moment from 'node-karin/moment'
 import karin, { common, logger, segment } from 'node-karin'
+import { Client } from 'icqq'
 
 /**
  * 改群名
@@ -86,3 +87,26 @@ export const SetEssence = karin.command(/^#?(加|设|移)精$/, async (e) => {
 
   return true
 }, { name: '处理精华消息', priority: -1, event: 'message.group', perm: 'group.admin' })
+
+export const segGroupAvatar = karin.command(/^#(改|设置|修改)群头像/i, async (e) => {
+  try {
+    if (!await isAdmin(e)) return false
+    let file = e.elements.find(item => item.type === 'image')?.file
+    if (!file) {
+      await e.reply('请发送需要更换的图片')
+      const event = await karin.ctx(e)
+      file = event.elements.find(item => item.type === 'image')?.file
+      if (!file) return await e.reply('未检测到图片')
+    }
+    if (e.bot.adapter.standard === 'icqq') {
+      await (e.bot.super as Client).pickGroup(Number(e.groupId)).setAvatar(file)
+    } else if (e.bot.adapter.standard === 'onebot11') {
+      await e.bot.super.sendApi!('set_group_portrait', { group_id: e.groupId, file })
+    } else return await e.reply(`当前适配器[${e.bot.adapter.standard}]暂不支持此功能`)
+    return await e.reply('修改成功')
+  } catch (error) {
+    await e.reply('未知错误\n请前往控制台查看')
+    logger.error(error)
+    return false
+  }
+}, { name: '修改群头像', priority: -1, event: 'message.group', perm: 'group.admin' })
