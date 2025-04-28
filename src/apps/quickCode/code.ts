15ff9d62cadabe1f5353a4a176a016d6a0c3f8b5
瑜笙 <2624367622@qq.com>
2025-04-28 19:22:48 +0800

    fix: 移除全局 Function 对象定义，增强代码安全性并优化 process 对象的创建

--- src/apps/quickCode/code.ts
+++ src/apps/quickCode/code.ts
@@ -4,12 +4,6 @@ import * as karin from 'node-karin'
 export const runjs = karin.karin.command(/^rjs/, async (e) => {
   const code = e.msg.replace(/^rjs/, '').trim()
   if (!code) return false
-  Object.defineProperty(global, 'Function', {
-    value: undefined,
-    configurable: false,
-    writable: false,
-    enumerable: false
-  })
   try {
     const sandbox = {
       ...karin,
@@ -24,15 +18,18 @@ export const runjs = karin.karin.command(/^rjs/, async (e) => {
       Buffer,
       global,
       globalThis,
-      process: {
-        env: process.env,
-        pid: process.pid,
-        argv: process.argv,
-        platform: process.platform,
-        version: process.version,
-        versions: process.versions
-      },
+      process: Object.create(null),
     }
+    const createprocess = ['env', 'cwd', 'pid', 'platform', 'version', 'versions', 'argv']
+    createprocess.forEach(prop => {
+      (sandbox.process as any)[prop] = process[prop as keyof NodeJS.Process]
+    })
+    sandbox.process.env = Object.create(null)
+    for (const key in process.env) {
+      sandbox.process.env[key] = process.env[key]
+    }
+    Object.freeze(sandbox.process)
+    Object.freeze(sandbox.process.env)
     const result = await RunJs(code, sandbox)
     if (result === '') return e.reply('没有返回值')
     const msg = typeof result === 'object' && result !== null ? JSON.stringify(result, null, 2) : String(result)
