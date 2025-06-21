32e913cf06afe967c8313715d1776e7f068b9de5
瑜笙 <2624367622@qq.com>
2025-06-21 08:19:20 +0800

    fix: 修改群昵称相关命令为群名片，更新依赖项 (#185)

--- src/apps/quickCode/file.ts
+++ src/apps/quickCode/file.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import { karin, common, logger } from 'node-karin'
+import { Size } from '@/components/Size'
 
 export const FileDownload = karin.command(/^文件下载/, async (e) => {
   e.reply('请发送文件', { at: true })
@@ -13,7 +14,7 @@ export const FileDownload = karin.command(/^文件下载/, async (e) => {
   }
   let url
   if (!e.isGroup) {
-    url = await (e.bot as any).super.pickFriend(e.userId).getFileUrl(file.fid)
+    url = e.bot.adapter.protocol === 'icqq' ? await (e.bot as any).super.pickFriend(e.userId).getFileUrl(file.fid) : ''
   } else {
     url = await (e.bot as any).super.pickGroup(e.groupId).getFileUrl(file.fid)
   }
@@ -56,3 +57,23 @@ export const UploadDownload = karin.command(/^文件上传/, async (e) => {
   }
   return true
 }, { name: '文件上传', priority: -1, permission: 'master' })
+
+export const getfile = karin.command(/^#(取|get)文件直链$/i, async (e) => {
+  if (!e.replyId) return e.reply('请回复需要获取直链的文件消息', { at: true })
+  const msg = await e.bot.getMsg(e.contact, e.replyId)
+  const file = JSON.parse((msg.elements.find(item => item.type === 'text'))!.text)
+  if (file.type !== 'file') {
+    return e.reply('未识别到文件', { at: true })
+  }
+  let url
+  if (!e.isGroup) {
+    url = await (e.bot as any).super.pickFriend(e.userId).getFileUrl(file.fid)
+  } else {
+    url = await (e.bot as any).super.pickGroup(e.groupId).getFileUrl(file.fid)
+  }
+  if (!url) {
+    e.reply('获取链接失败', { at: true })
+    return true
+  }
+  return await e.reply(`文件名: ${file.name}\n文件大小: ${await Size(file.size)}\n文件链接: ${url}`, { at: true })
+})
